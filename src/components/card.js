5c6ac2b76dc3abf0ba5cd0e032b6fee3ebee8eca
Abdul Jawad Azizi <jawadazizi052@gmail.com>
2024-08-27 10:04:28 +0700

    New contact accent

--- src/components/card.js
+++ src/components/card.js
@@ -24,13 +24,13 @@ const Card = ({ children }) => {
   return (
     <div
       onMouseMove={onMouseMove}
-      className="card-container cursor-pointer overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600"
+      className="card-container cursor-pointer overflow-hidden relative duration-700 border rounded-xl hover:bg-purple-600/10 group md:gap-8 hover:border-purple-500/50 border-pink-500"
     >
       <div className="pointer-events-none">
         <div className="absolute inset-0 z-0 transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
         {/* First animated layer */}
         <motion.div
-          className="absolute inset-0 z-10 bg-gradient-to-br opacity-100 via-zinc-100/10 transition duration-1000 group-hover:opacity-50"
+          className="absolute inset-0 z-10 bg-gradient-to-br opacity-100 via-purple-400/10 transition duration-1000 group-hover:opacity-50"
           style={style}
         />
         {/* Second animated layer */}
